2b1062062631080deb2411f69702ab06d37d13fb
Pete Hanson <wolfy@pdxwolfy.org>
2019-03-04 00:54:24 -0800

    Add root_url test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,6 +5,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
+  test "should get root" do
+    get root_url
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
   test "should get about" do
     get static_pages_about_url
     assert_response :success
