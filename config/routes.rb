c38b8a5fbb68e35b2a6e0044da4d02a497736b76
Pete Hanson <wolfy@pdxwolfy.org>
2019-03-05 05:27:12 -0800

    Finish up chapter

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get "users/new"
+
   root "static_pages#home"
 
   get "/about",   to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
   get "/home",    to: "static_pages#home"
   get "/help",    to: "static_pages#help"
+  get "/signup",  to: "users#new"
 end
